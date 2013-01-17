fdb31414756c675ce74c402d3f77d004e7c92ffd
王健 <wangjian2254@gmail.com>
2013-01-17 17:23:39 +0800

    用户订阅股票 接口开发（未完成）

--- im/gupiaointerface.py
+++ im/gupiaointerface.py
@@ -1,5 +1,8 @@
 #coding=utf-8
+from im.model.models import GupiaoToGroup
+from im.rssinterface import memacheGroup
 from im.tool import getorAddUser
+import setting
 
 __author__ = 'Administrator'
 
@@ -38,9 +41,9 @@ class SearchGuPiao(Page):
                 opts=opt.split(',')
 #                self.response.out.write(u"%s</br>"%opts[3])
 #                self.response.out.write("%s</br>"%chardict[opts[1]])
-                if len(opts)<2:
-                    self.response.out.write(json.dumps(opts))
-                else:
+#                if len(opts)<2:
+#                    self.response.out.write(json.dumps(opts))
+                if opts[1] in ['11','12','31','41']:
                     l.append({'key':opts[0],'type_n':opts[1],'type':chardict[opts[1]],'dm':opts[2],'dmall':opts[3],'name':opts[4]})
 #                    self.response.out.write(u"%s %s %s %s %s %s</br>"%(opts[0],chardict[opts[1]],opts[2],opts[3],opts[4],opts[5]))
 
@@ -60,7 +63,55 @@ class JoinGuPiao(Page):
             dmall=self.request.get("dmall")
             type_n=self.request.get("type_n")
             name=self.request.get("name")
-            #### 下面是加入群
+            ###操作类型
+            do=self.request.get('DoType')# '1'为加入 '2'为退订
+            groupId=self.request.get('GroupId')
+            if do=='1':
+                #### 下面是加入群
+                realNo=dmall
+                if type_n in ['11','12']:
+                    pass
+                elif type_n == '31':
+                    realNo='hk'+dm
+                elif type_n=='41':
+                    realNo='gb_'+dm
+                else:
+                    self.response.out.write('1')
+                    return
+                gupiaoToGroup=GupiaoToGroup.get_by_key_name(key_names=dm)
+                if gupiaoToGroup:
+                    group=memacheGroup(groupId)
+                    if group:
+                        if userName not in group.member:
+                            group.member.append(userName)
+                        if group.key().id() not in user.memberGroup:
+                            if group.key().id() not in user.memberGroupAdd:
+                                user.memberGroupAdd.append(group.key().id())
+                                if group.key().id() in user.memberGroupRemove:
+                                    user.memberGroupRemove.remove(group.key().id())
+                    else:
+                        self.response.out.write('2')
+                else:
+                    pass#新被订阅的股票
+            elif do=='2':
+                ##### 下面是退出群 退订股票
+                if groupId:
+                    try:
+                        groupId=int(groupId)
+                    except Exception,e:
+                        logging.info(str(e))
+                        self.response.out.write('1')
+                        return
+                    group=memacheGroup(groupId)
+                    if userName in group.member:
+                        group.member.remove(userName)
+                    if group.key().id() in user.memberGroup:
+                        user.memberGroup.remove(group.key().id())
+                        if group.key().id() not in user.memberGroupRemove:
+                            user.memberGroupRemove.append(group.key().id())
+                            if group.key().id() in user.memberGroupAdd:
+                                user.memberGroupAdd.remove(group.key().id())
+            self.response.out.write(setting.WEBURL[7:]+'/InfoUpdate')
         except Exception,e:
             logging.info(str(e))
             self.response.out.write('1')
