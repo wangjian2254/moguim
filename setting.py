616e9cae075d3cf28956e3eacbd7635298b670ff
王健 <wangjian2254@gmail.com>
2013-03-17 22:47:34 +0800

    优化

--- setting.py
+++ setting.py
@@ -11,13 +11,12 @@ TEMPLATE_DIR=os.path.dirname(__file__)
 #应用code
 APPCODE='a4'
 #WEBURL='http://192.168.1.104:8080'
-WEBURL='http://group.mmggoo.com'
-#WEBURL='http://im.zxxsbook.com'
-
+WEBURL='http://im.zxxsbook.com'
+#WEBURL='http://group.mmggoo.com'
 
 APPCODE_ID='a4_1'
 
-HOMEURL='http://www.mmggoo.com'
+HOMEURL='http://blog.zxxsbook.com'
 
 APPCODE_APPTYPE={'0':u'新闻订阅','1':u'聊天','3':u'群','4':u'股票','999':u'系统信息'}
 
@@ -27,6 +26,10 @@ AdNoteTime='20120903'
 #股票同步应用网址（获取股票数据的应用）
 #GUPIAOURL='http://gupiao.mmggoo.com'
 #GUPIAOURL='http://localhost:8080'
-GUPIAOURL='http://sz-gupiao.appspot.com'
+GUPIAOURL='http://smail-joke2.appspot.com'
+
+
+
+
 
 
