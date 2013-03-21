b050bbc8e8e6509fc7b34195ddae5c80f0939322
王健 <wangjian2254@gmail.com>
2013-03-22 07:00:09 +0800

    开始开发 微论坛 接口

---
+++ im/weigroupinterface.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+#author:u'王健'
+#Date: 13-3-21
+#Time: 下午6:51
+from tools.page import Page
+
+__author__ = u'王健'
+
+class CreateWeiNote(Page):
+    def get(self):
+        pass
+    def post(self):
+        pass
+
+
+
+class UploadPic(Page):
+    def post(self):
+        pass
